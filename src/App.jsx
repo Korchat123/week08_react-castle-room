211a4d4a18213e9d598308aa0f260d0c8a0a445d
Korchat123 <korchat123@gmail.com>
2026-04-21 16:00:05 +0700

    add label by Array

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,26 @@ const handleQuestion=(e)=>{
 setQuestion(e.target.value)
 };
 
-const [question2,setQuestion2]= useState("");
-const handleQuestion2=(e)=>{
-setQuestion2(e.target.value)
+const [answer,setAnswer]= useState("");
+const handleAnswer=(e)=>{
+setAnswer(e.target.value)
 };
-
-
+const arrLabel=["Message for secret","Message for outside","reply from secret room: ","send message: ","Message from outside: ","Reply Message: "];
+const factorI=0
 const Inputoutside=<Inputbox handleQuestion={handleQuestion} 
+                              factorI={0}
                               question={question} 
-                              text="message to Secretroom : "
-                              question2={(question2)?question2:"wait for message from secret room"}
-                              text2={question?question:"wait input message to secret room"}
+                              textLabel={arrLabel}
+                              question2={(answer)?answer:"wait for message from secret room...."}
+                              myMessage={question?question:"wait input message to secret room...."}
                               >                               
                      </Inputbox>;
-const Inputinside=<Inputbox handleQuestion={handleQuestion2} 
-                              question={question2} 
-                              text="message to Outside : "
-                              question2={question?question:"wait for message from outside"}
-                              text2={question2?question2:" wait input reply message to outside"}
+const Inputinside=<Inputbox handleQuestion={handleAnswer} 
+                              factorI={1}
+                              question={answer} 
+                              textLabel={arrLabel}
+                              question2={question?question:"wait for message from outside....."}
+                              myMessage={answer?answer:" wait input reply message to outside....."}
                               >         
                      </Inputbox>;
 
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,21 +1,29 @@
 
-export default function Inputbox({handleQuestion,question,question2,text,text2}){
+export default function Inputbox({factorI,handleQuestion,question,question2,textLabel,myMessage}){
 return( 
 
     <div className="pb-[10vh] py-[10vh] gap-y-4 flex flex-col justify-center w-full items-center h-full bg-gray-800 text-white">
     <p className="text-purple-300">
-        {text}
+         {textLabel[factorI*1]}
    </p>
    <textarea
       onChange={handleQuestion}
       value={question} 
       className="bg-white text-black rounded px-2 py-1" 
       placeholder="TYPE your message...."/>
-
-    <span className="text-yellow-300">
+        
+   <p> 
+        {textLabel[factorI+2]}
+        <span className="text-yellow-300">
         {question2}
       </span>  
-    <p> {text2}</p> 
+      </p>
+
+    <p>
+        {textLabel[factorI+4]}
+        <span> {myMessage}</span> 
+    </p>
+
  </div>
    )
 
